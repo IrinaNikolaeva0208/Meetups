049603bdb3c84237a25d36fe7a69f23b5e192083
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-09-13 16:34:03 +0300

    add user model to database

--- prisma/migrations/20230913130753_init/migration.sql
+++ prisma/migrations/20230913130753_init/migration.sql
@@ -9,3 +9,15 @@ CREATE TABLE "Meetup" (
 
     CONSTRAINT "Meetup_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_login_key" ON "User"("login");
