919a7bd20c4809bdb27977f3f8b4a3f22387bafa
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-09-21 17:34:37 +0300

    make validation stricter

--- src/validation/schemas/createMeetup.schema.ts
+++ src/validation/schemas/createMeetup.schema.ts
@@ -1,9 +1,9 @@
 import Joi from "joi";
 
 export const CreateMeetupSchema = Joi.object().keys({
-  name: Joi.string().max(40).required(),
-  description: Joi.string().max(300).required(),
-  tags: Joi.array().items(Joi.string()).max(10).required(),
+  name: Joi.string().alphanum().max(40).required(),
+  description: Joi.string().alphanum().max(300).required(),
+  tags: Joi.array().items(Joi.string().alphanum()).max(10).required(),
   time: Joi.string().isoDate().required(),
-  place: Joi.string().max(40).required(),
+  place: Joi.string().alphanum().max(40).required(),
 });
--- src/validation/schemas/updateMeetup.schema.ts
+++ src/validation/schemas/updateMeetup.schema.ts
@@ -1,9 +1,9 @@
 import Joi from "joi";
 
 export const UpdateMeetupSchema = Joi.object().keys({
-  name: Joi.string().max(40).optional(),
-  description: Joi.string().max(300).optional(),
-  tags: Joi.array().items(Joi.string()).max(10).optional(),
+  name: Joi.string().alphanum().max(40).optional(),
+  description: Joi.string().alphanum().max(300).optional(),
+  tags: Joi.array().items(Joi.string().alphanum()).max(10).optional(),
   time: Joi.date().optional(),
-  place: Joi.string().max(40).optional(),
+  place: Joi.string().alphanum().max(40).optional(),
 });
