1d409bfa35f293c1476ef081574fb284e7e735b1
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-09-22 16:24:59 +0300

    delete alphanum validation from description property schema

--- src/validation/schemas/createMeetup.schema.ts
+++ src/validation/schemas/createMeetup.schema.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const CreateMeetupSchema = Joi.object().keys({
   name: Joi.string().alphanum().max(40).required(),
-  description: Joi.string().alphanum().max(300).required(),
+  description: Joi.string().max(300).required(),
   tags: Joi.array().items(Joi.string().alphanum()).max(10).required(),
   time: Joi.string().isoDate().required(),
   place: Joi.string().alphanum().max(40).required(),
