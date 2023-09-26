ef631c4b7d187eeeee0f11e428101a664cb4e2ab
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-09-26 11:45:38 +0300

    implement error-handling middleware

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import authRouter from "./auth/routers/auth.router";
 import checkIfTokenIsValid from "@authorization/checkIfTokenIsValid";
 import { getUserByJwt } from "./user/getUserByJwt";
 import cors from "cors";
+import { createResponse } from "@responses/createResponse";
 
 const PORT = envVars.PORT || 4000;
 
@@ -19,6 +20,11 @@ meetupsApp.use("/meetups", checkIfTokenIsValid, meetupsRouter);
 meetupsApp.use("/user", checkIfTokenIsValid, getUserByJwt);
 meetupsApp.all("*", (req, _, next) => {
   next(createError(404, `Cannot find ${req.originalUrl}`));
+});
+meetupsApp.use((err, _, res, next) => {
+  if (res.headersSent) return next(err);
+  const response = createResponse(err.status || err.statusCode || 500, err);
+  res.status(response.statusCode).json(response);
 });
 
 meetupsApp.listen(PORT, () => console.log("Server started on port " + PORT));
