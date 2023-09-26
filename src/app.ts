f750fdd3c8d5772e6f50cae5308ed505951074a3
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-09-26 10:32:52 +0300

    add processing non-existing endpoinds middleware

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,7 @@ import "module-alias/register";
 import express from "express";
 import { envVars } from "@environment";
 import meetupsRouter from "./meetups/routers/meetups.router";
-import { sendInvalidRouteResponse } from "@validators/sendInvalidRouteResponse";
-import { validateJSON } from "@validators/validateJSON";
+import createError from "http-errors";
 import authRouter from "./auth/routers/auth.router";
 import checkIfTokenIsValid from "@authorization/checkIfTokenIsValid";
 import { getUserByJwt } from "./user/getUserByJwt";
@@ -14,10 +13,12 @@ const PORT = envVars.PORT || 4000;
 const meetupsApp = express();
 
 meetupsApp.use(cors());
-meetupsApp.use(validateJSON);
+meetupsApp.use(express.json());
 meetupsApp.use("/auth", authRouter);
 meetupsApp.use("/meetups", checkIfTokenIsValid, meetupsRouter);
 meetupsApp.use("/user", checkIfTokenIsValid, getUserByJwt);
-meetupsApp.use(sendInvalidRouteResponse);
+meetupsApp.all("*", (req, _, next) => {
+  next(createError(404, `Cannot find ${req.originalUrl}`));
+});
 
 meetupsApp.listen(PORT, () => console.log("Server started on port " + PORT));
