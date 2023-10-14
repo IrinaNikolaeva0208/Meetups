735da3c485b916ecd39a7e40daee4c85b2236868
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-10-14 23:23:22 +0300

    add check if admin already exists to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,16 +7,21 @@ import { Roles } from "../src/utils/interfaces/roles.enum";
 const database = new PrismaClient();
 
 async function main() {
-  await database.user.create({
-    data: {
-      login: envVars.ADMIN_LOGIN,
-      password: await bcrypt.hash(
-        envVars.ADMIN_PASSWORD as string,
-        +(envVars.CRYPT_SALT as string)
-      ),
-      roles: [Roles.admin, Roles.meetup_organizer, Roles.user],
-    },
+  const adminAlreadyExists = await database.user.findUnique({
+    where: { login: envVars.ADMIN_LOGIN },
   });
+
+  if (!adminAlreadyExists)
+    await database.user.create({
+      data: {
+        login: envVars.ADMIN_LOGIN,
+        password: await bcrypt.hash(
+          envVars.ADMIN_PASSWORD as string,
+          +(envVars.CRYPT_SALT as string)
+        ),
+        roles: [Roles.admin, Roles.meetup_organizer, Roles.user],
+      },
+    });
 }
 
 main()
