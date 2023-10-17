3f4aa68db81441a5f1ff3e3621130561e35399eb
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2023-10-18 02:49:06 +0300

    rewrite formPaginationOptions helper

--- src/meetups/app/schemas/paginationQueryParams.schema.ts
+++ src/meetups/app/schemas/paginationQueryParams.schema.ts
@@ -3,10 +3,11 @@ import Joi from "joi";
 export const PaginationQueryParamsSchema = Joi.object().keys({
   offset: Joi.number().min(0).required(),
   limit: Joi.number().min(1).required(),
-  sort: Joi.string().valid("name", "time", "place"),
-  order: Joi.string().valid("asc", "desc"),
-  time: Joi.string().isoDate(),
-  place: Joi.string().alphanum(),
-  tags: Joi.string(),
-  search: Joi.string().alphanum(),
+  sort: Joi.string().valid("name", "time"),
+  order: Joi.string().valid("asc", "desc").optional(),
+  time: Joi.string().isoDate().optional(),
+  latitude: Joi.number().min(-90).max(90).optional(),
+  longtitude: Joi.number().min(-180).max(180).optional(),
+  tags: Joi.string().optional(),
+  search: Joi.string().alphanum().optional(),
 });
